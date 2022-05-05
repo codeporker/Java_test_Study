6145891a9216dca70cc65976407b33dfd8f98edc
porker <2207894510@qq.com>
2022-05-05 14:06:10 +0800

    first commit

---
+++ test/src/day1/test1.java
@@ -0,0 +1,13 @@
+package day1;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Aurher:Porker_ZHU
+ * @Date:2022/5/4 - 05 - 04 - 15:30
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println('a'+'b');
+    }
+}
